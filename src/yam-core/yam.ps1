1a59f2dffd52fd46d7501d6f988ff5b1feeac3f8
xiaoyvr <xiaoyvr@gmail.com>
2012-10-07 20:43:57 +0800

    yam config

--- src/yam-core/yam.ps1
+++ src/yam-core/yam.ps1
@@ -7,9 +7,49 @@ if ($ptrSize = 8) {
 }
 $version = 'v4.0.30319'
 $msbuild = "$env:windir\Microsoft.NET\Framework$bitness\$version\MSBuild.exe"
-$project = "C:\e x b\yam-c ore.csproj"
-# GetProjectOutputItems
-$result = &$msbuild yam.targets /t:GetProjectOutput /p:project=$project /nologo /v:m 
-write-host $result -f yellow
 
+# GetProjectOutputItems
 # project regex in solution Project\("\{(?<Type>.{36})\}"\)\s*=\s*"(?<Name>.*)",\s*"(?<Path>.*)",\s*"\{(?<Id>.{36})\}"
+
+$root = $MyInvocation.MyCommand.Path | Split-Path -parent
+
+$codebaseRoot = "."
+$codebaseConfig = @{
+    'projectDirs' = @("$codebaseRoot\src", "$codebaseRoot\test") 
+    'libDirs' = @("$codebaseRoot\libs", "$codebaseRoot\packages")
+}
+
+switch ($command){
+    'config' {
+        $prjs = $codebaseConfig.projectDirs | ? { Test-Path $_} | Get-ChildItem -Recurse -filter *.csproj | % { $_.FullName }
+        $results = $prjs | % { 
+            $output = &$msbuild $root\yam.targets /t:GetProjectOutput /p:project=$_ /nologo /v:m
+            @{
+                "Output" = $output.trim()
+                "Project" = Resolve-Path $_ -Relative
+            }
+        } 
+        if ($LastExitCode -ne 0) {
+            throw "error: $outputs"
+        }
+        if (Test-Path $codebaseRoot\prj.config) {
+            Remove-Item $codebaseRoot\prj.config    
+        }
+        $results | %{ 
+            $fileName = [System.IO.Path]::GetFileNameWithoutExtension($_.Output)
+            $prj = Resolve-Path $($_.Project) -Relative
+            Add-Content $codebaseRoot\prj.config "Project, $fileName, $prj" 
+        }
+
+        $libs = $codebaseConfig.libDirs | ? { Test-Path $_} | Get-ChildItem -Recurse -filter *.dll | % { $_.FullName }
+        $libs | %{ 
+            $fileName = [System.IO.Path]::GetFileNameWithoutExtension($_)
+            $prj = Resolve-Path $_ -Relative
+            Add-Content $codebaseRoot\prj.config "Lib, $fileName, $prj" 
+        }
+    }
+    'help'{
+
+    }
+}
+
